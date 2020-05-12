7700beb7c65c4b7e52e26cdb719db8fde5de6124
JyotinderSingh <jyotindrsingh@gmail.com>
2020-05-12 18:52:33 +0530

    train function added to Network wrapper

--- __Network.py
+++ __Network.py
@@ -7,19 +7,66 @@ import Optimizers
 
 
 class Network:
+    '''Network(inputFeatures)
+    Creates a new Neural Network object.
+    inputFeatures: Dimensions per sample
+    '''
+
+
     def __init__(self, inputFeatures):
         self.layers = []
         self.inputFeatures = inputFeatures
         self.prev = -1
-    
+
     def forwardPass(self):
         pass
 
     def backwardPass(self):
         pass
 
-    def train(self, epochs):
-        pass
+    def train(self, input, labels, epochs, lossFunction, optimizer):
+        '''Performs a forward pass on the input data through the network for the
+        specified number of epochs.
+        parameters:
+        input: np.array object\n
+        labels: np.array object\n
+        epochs: integer\n
+        lossFunction: instance of some loss function (eg. Loss_CategoricalCrossEntropy)\n
+        optimizer: instance of an optimizer (eg. Adam, AdaGrad, SGD etc.)
+        '''
+
+        assert input.shape[1] == self.inputFeatures
+        assert len(self.layers) > 0
+
+        for epoch in range(epochs):
+            inputLayerOutput = self.layers[0].forward(input)
+
+
+            # Forward pass
+            for idx in range(1, len(self.layers)):
+                self.layers[idx].forward(self.layers[idx - 1].output)
+
+            # Get metrics
+            loss = lossFunction.forward(self.layers[-1].output, labels)
+            accuracy = Model.model_accuracy(self.layers[-1].output, labels)
+
+            if not epoch % 100:
+                print(
+                    f'\nepoch: {epoch}, acc: {accuracy:.3f}, loss: {loss:.3f}, lr: {optimizer.current_learning_rate}')
+
+            # Backward pass
+            lossFunction.backward(self.layers[-1].output, labels)
+            self.layers[-1].backward(lossFunction.dvalues)
+            for idx in range(len(self.layers) - 2, -1, -1):
+                self.layers[idx].backward(self.layers[idx + 1].dvalues)
+
+            # Update weights
+            optimizer.pre_update_params()
+            for idx in range(len(self.layers)):
+                if isinstance(self.layers[idx], Layers.Layer_Dense):
+                    optimizer.update_params(self.layers[idx])            
+            optimizer.post_update_params
+
 
     def addDenseLayer(self, neurons):
         if len(self.layers) == 0:
--- __NetworkDemo.py
+++ __NetworkDemo.py
@@ -1,4 +1,9 @@
 from __Network import Network
+from Loss import Loss_CategoricalCrossEntropy
+from Optimizers import Optimizer_Adam
+from Model import create_data
+
+X, y = create_data(100, 3)
 
 net = Network(2)
 net.addDenseLayer(64)
@@ -8,3 +13,8 @@ net.addReLU()
 net.addDenseLayer(3)
 net.addSoftmax()
 print(net.getSummary())
+
+loss_function = Loss_CategoricalCrossEntropy()
+optimizer = Optimizer_Adam(learning_rate=0.05, decay=4e-8)
+
+net.train(X, y, 10001, loss_function, optimizer)
